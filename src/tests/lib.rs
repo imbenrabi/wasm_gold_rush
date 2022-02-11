351f7e2ee6c7a2fda925e4dee87102f41ce592bf
Ben Rabi <benr@wix.com>
2022-02-11 19:58:55 +0200

    add(tests): create board

--- src/tests/lib.rs
+++ src/tests/lib.rs
@@ -4,7 +4,9 @@ use matrix;
 mod tests {
     #[test]
     fn should_return_new_board() {
+        let value = vec![vec![0; 3]; 3]
         let board = matrix::generate_new_board(3, 3);
         matrix::print_board(board);
+        assert_eq!(board, value);
     }
 }
