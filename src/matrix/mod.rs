24805905445d647c5e15779e72b4b58cf0696f2b
Ben Rabi <benr@wix.com>
2022-02-15 16:56:25 +0200

    add(matrix): test setup matrix data type

--- src/matrix/mod.rs
+++ src/matrix/mod.rs
@@ -1,36 +1,44 @@
 pub mod matrix {
     use wasm_bindgen::prelude::*;
     
-    pub type Board = Vec<Vec<usize>>;
-    
+    pub type MatrixData = Vec<Vec<i32>>;
+
     #[derive(Debug)]
     #[wasm_bindgen]
-    pub struct Matrix {
-        columns: usize,
-        rows: usize,
-        data: Board,
+    pub struct Matrix  {      
+        data: MatrixData,
     }
     
     impl Matrix {
         fn new(columns: usize, rows: usize) ->  Matrix {
-            let mut matrix = Matrix {
-                columns: columns,
-                rows: rows,
+            let matrix = Matrix {              
                 data: vec![vec![0; columns]; rows]
             };
             matrix
         }
 
         fn get(&self) -> &Matrix {
-            self
+            &self
+        }
+
+        fn view(&self) -> &MatrixData {
+            &self.data
         }
     }
 
-    pub fn create_new_matrix(cols: usize, rows: usize) -> Matrix {
+    pub fn create_new_matrix(cols: usize, rows: usize) -> Matrix    {
         Matrix::new(cols, rows)
     }
 
-    pub fn print_matrix(matrix: Matrix) {
+    pub fn print_matrix(matrix: &Matrix) {
         println!("{:?}", matrix.get());
+    }
+
+    #[test]
+    fn should_get_new_board() {
+        let value: MatrixData = vec![vec![0; 3]; 3];
+        let board = create_new_matrix(3, 3);
+        print_matrix(&board);
+        assert_eq!(&board.data, &value);
     }
 }
